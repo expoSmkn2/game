61aee4f2655cbbf50bdd94a2031b5b8c5662b045
Akmal Dzaki <akmaldzakih@gmail.com>
2025-12-16 19:01:34 +0700

    0.8

--- assets/scripts/player_ctrl.gd
+++ assets/scripts/player_ctrl.gd
@@ -1,40 +1,116 @@
 extends CharacterBody2D
-class_name controller
 
-@onready var sprite = $animator/Sprite2D
-@export var speed = 10.0
-@export var jump_power = 10.0
+@onready var animation = $animator/animation
+@onready var deal_damage_zone = $DealDamageZone
 
-var speed_multiplier = 30.0
-var jump_multiplier = -30.0
-var direction = 0
-var attacking = false
+@export var w_speed = 150
+@export var l_speed = 50
+@export var a_speed = 75
+var speed = 150
+var speed_atk = 0 
+const  jump_power = -290
 
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") 
+var gravity = 900
+var was_running = false
+
+var is_light_attack := false
+var is_heavy_attack := false
+
+var is_double_jump_attack := false
+
+var attack_type: String
+var current_attack: bool
+
 
 func _physics_process(delta):
-	# Add the gravity.
-	if Input.is_action_just_pressed("attack"):
-		attacking = true
-		speed = 3
-	if Input.is_action_pressed("ml"):
-		sprite.scale.x = abs(sprite.scale.x) * -1
-	if Input.is_action_pressed("mr"):
-		sprite.scale.x = abs(sprite.scale.x)
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jp") and is_on_floor():
-		velocity.y = jump_power * jump_multiplier
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ml", "mr")
+		velocity.y += gravity * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_power
+	
+	if Input.is_action_just_pressed("jump") and !is_on_floor() and !is_double_jump_attack:
+		velocity.y = jump_power / 1.25
+		is_double_jump_attack = true
+	
+	var  direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * speed * speed_multiplier
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
+		velocity.x = direction * speed
+	else: 
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	if !current_attack:
+		if Input.is_action_just_pressed("M2") and is_on_floor():
+			current_attack = true
+			is_heavy_attack = true
+			attack_type = "heavy_attack"
+
+		elif Input.is_action_just_pressed("M1") and is_on_floor():
+			current_attack = true
+			is_light_attack = true
+			attack_type = "light_attack"
+	
+		elif Input.is_action_just_pressed("M1") and !is_on_floor() and !is_double_jump_attack:
+			current_attack = true
+			is_light_attack = true
+			is_double_jump_attack = true
+			attack_type = "air"
+			velocity.y = jump_power / 1.25
+		
+		
+
+	if current_attack:
+		handle_attack_animation(attack_type)
+		Global.playerBody = self
+		
+	if is_on_floor() and !current_attack:
+		is_double_jump_attack = false
+		if abs(velocity.x) > 1:
+			animation.play("run")
+		else:
+			animation.play("idle")
+	
 	move_and_slide()
+	handle_movement_animation(direction)
+	
+	
+	
+func _ready():
+	animation.connect("animation_finished", Callable(self, "_on_animation_finished"))
+	Global.playerBody = self
+	current_attack = false
+	
+func _on_animation_finished():
+	if animation.animation in ["light_attack", "heavy_attack", "air"]:
+		current_attack = false
+		is_light_attack = false
+		is_heavy_attack = false
+		speed = (speed * speed_atk) + w_speed
+
+		if is_on_floor():
+			animation.play("idle")
+		else:
+			animation.play("fall")
+func handle_movement_animation(dir):
+	if is_heavy_attack:
+		return
+
+	if current_attack and is_light_attack and !is_on_floor():
+		return
+	toggle_flip_sprite(dir)
+func toggle_flip_sprite(dir):
+	if dir == 1:
+		animation.flip_h = false
+	if dir == -1:
+		animation.flip_h = true
+		
+
+func handle_attack_animation(attack_type):
+	if current_attack:
+		speed = speed * speed_atk
+		var anim = str(attack_type)
+		animation.play(anim)
+		if str(attack_type) == "light_attack":
+			speed = l_speed
+		elif str(attack_type) == "air":
+			speed = a_speed
